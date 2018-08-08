64ea8a59315bea48d3642f66ce7fa21172d4ce72
Gökhan Ercan <gokhan@ercannet.com>
2018-08-08 22:01:21 +0300

    redundant jetty print

--- PackageWebServer.bat
+++ PackageWebServer.bat
@@ -3,5 +3,4 @@ echo "Packaging vaadin web server app into the war file..."
 rem call mvn clean 'often requires cleaning in order to remove older jar references'
 rem call mvn compile 'still it can't compile via mvn standalone, we depend upon IDE's compilation settings!'
 call mvn package
-echo jetty:run
 pause
